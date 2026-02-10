8e3a3a98e9ade608dafe71f812f03236a12878aa
Abhinav Prakash <abprakash85409@gmail.com>
2026-02-10 17:42:10 +0530

    chore: remove  express folder after submodule migration

--- store/stacks/express/src/utils/setTokenCookies.js
+++
@@ -1,8 +0,0 @@
-import { cookieOptions } from '../../config/cookie.js';
-
-const setTokenCookies = (res, tokens) => {
-  res.cookie('accessToken', tokens.accessToken, cookieOptions.accessToken);
-  res.cookie('refreshToken', tokens.refreshToken, cookieOptions.refreshToken);
-};
-
-export default setTokenCookies;
